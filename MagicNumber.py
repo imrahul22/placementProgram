75ff1bd792799e69a64b558a305aa542cbff8641
Rahul Mondal <67455254+imrahul22@users.noreply.github.com>
2021-06-24 20:07:59 +0530

    Add files via upload

---
+++ MagicNumber.py
@@ -0,0 +1,17 @@
+#Magic number.
+
+def isMagic(n):
+    sum = 0;
+    while (n > 0 or sum > 9):
+        if (n == 0):
+            n = sum;
+            sum = 0;
+        sum = sum + n % 10;
+        n = int(n / 10);
+    return True if (sum == 1) else False;
+
+n = int(input())
+if (isMagic(n)):
+    print("Magic Number");
+else:
+    print("Not a magic Number");
